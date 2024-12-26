eee3219fce31c86c2c78d731359db94232f0bcad
shyam-amahahealth <shyam02062001@gmail.com>
2024-12-26 16:25:51 +0530

    all diyar land marker and one round path

---
+++ constants/paths.ts
@@ -0,0 +1,253 @@
+export const PATHS = [
+  {latitude: 25.62747, longitude: 84.4335},
+  {latitude: 25.62745, longitude: 84.43352},
+  {latitude: 25.627244, longitude: 84.433367},
+  {latitude: 25.62734, longitude: 84.4332},
+  {latitude: 25.62738, longitude: 84.43315},
+  {latitude: 25.62743, longitude: 84.433},
+  {latitude: 25.62756, longitude: 84.4328},
+  {latitude: 25.62762, longitude: 84.4327},
+  {latitude: 25.62782, longitude: 84.4325},
+  {latitude: 25.62798, longitude: 84.4322},
+  {latitude: 25.62816, longitude: 84.432},
+  {latitude: 25.62837, longitude: 84.43175},
+  {latitude: 25.62843, longitude: 84.43175},
+  {latitude: 25.62856, longitude: 84.4319},
+  {latitude: 25.62862, longitude: 84.432},
+  {latitude: 25.62883, longitude: 84.4323},
+  {latitude: 25.62908, longitude: 84.4326},
+  {latitude: 25.629356, longitude: 84.432822},
+  {latitude: 25.629534, longitude: 84.43262},
+  {latitude: 25.6299, longitude: 84.433},
+  {latitude: 25.63009, longitude: 84.4332},
+  {latitude: 25.630145, longitude: 84.43327},
+  {latitude: 25.63022, longitude: 84.4334},
+  {latitude: 25.6303, longitude: 84.4336},
+  {latitude: 25.63032, longitude: 84.4337},
+  {latitude: 25.63056, longitude: 84.434},
+  {latitude: 25.63081, longitude: 84.4342},
+  {latitude: 25.631055, longitude: 84.4344},
+  {latitude: 25.63119, longitude: 84.43455},
+  {latitude: 25.63126, longitude: 84.4347},
+  {latitude: 25.631356, longitude: 84.4349},
+  {latitude: 25.6315, longitude: 84.4351},
+  {latitude: 25.6318, longitude: 84.43528},
+  {latitude: 25.632, longitude: 84.43505},
+  {latitude: 25.63258, longitude: 84.43438},
+  {latitude: 25.63279, longitude: 84.4345},
+  {latitude: 25.6335, longitude: 84.43545},
+
+  //ghat pe
+  {latitude: 25.633638, longitude: 84.435173},
+
+  {latitude: 25.6335, longitude: 84.43545},
+  {latitude: 25.6335, longitude: 84.43545},
+  {latitude: 25.63279, longitude: 84.4345},
+
+  {latitude: 25.63302, longitude: 84.4341},
+  {latitude: 25.63322, longitude: 84.4338},
+  {latitude: 25.63345, longitude: 84.4335},
+  {latitude: 25.633815, longitude: 84.433},
+  {latitude: 25.6339, longitude: 84.43303},
+  {latitude: 25.63437, longitude: 84.4334},
+  {latitude: 25.6351, longitude: 84.43402},
+
+  //Baba ka bagiacha
+  {latitude: 25.635652, longitude: 84.432929},
+
+  {latitude: 25.6351, longitude: 84.43402},
+  {latitude: 25.6356, longitude: 84.43435},
+
+  //bagaicha
+  {latitude: 25.636133, longitude: 84.433418},
+
+  {latitude: 25.6356, longitude: 84.43435},
+  {latitude: 25.6357, longitude: 84.43429},
+  {latitude: 25.63695, longitude: 84.4352},
+  {latitude: 25.63667, longitude: 84.4357},
+
+  //Satiwad patla wala plot
+  {latitude: 25.636742, longitude: 84.435725},
+
+  {latitude: 25.63667, longitude: 84.4357},
+  {latitude: 25.63695, longitude: 84.4352},
+  {latitude: 25.63716, longitude: 84.43485},
+
+  //Satiwad
+  {latitude: 25.637403, longitude: 84.435019},
+
+  {latitude: 25.63716, longitude: 84.43485},
+  {latitude: 25.63754, longitude: 84.43415},
+  {latitude: 25.6377, longitude: 84.43382},
+  {latitude: 25.637855, longitude: 84.43385},
+  {latitude: 25.638135, longitude: 84.434},
+
+  //Kargaiha
+  {latitude: 25.638197, longitude: 84.433903},
+
+  {latitude: 25.638135, longitude: 84.434},
+  {latitude: 25.63905, longitude: 84.4345},
+  {latitude: 25.63977, longitude: 84.435},
+
+  //jabanwali
+  {latitude: 25.64004, longitude: 84.434293},
+
+  {latitude: 25.63977, longitude: 84.435},
+  {latitude: 25.63905, longitude: 84.4345},
+  {latitude: 25.638135, longitude: 84.434},
+  {latitude: 25.637855, longitude: 84.43385},
+  {latitude: 25.6377, longitude: 84.43382},
+  {latitude: 25.63807, longitude: 84.433},
+  {latitude: 25.64015, longitude: 84.42886},
+  {latitude: 25.6407, longitude: 84.42845},
+  {latitude: 25.641, longitude: 84.42845},
+  {latitude: 25.6414, longitude: 84.42849},
+  {latitude: 25.6417, longitude: 84.42842},
+  {latitude: 25.642, longitude: 84.4285},
+
+  //Parti pe
+  {latitude: 25.641954, longitude: 84.428619},
+
+  {latitude: 25.642, longitude: 84.4285},
+  {latitude: 25.6417, longitude: 84.42842},
+  {latitude: 25.6414, longitude: 84.42849},
+  {latitude: 25.641, longitude: 84.42845},
+  {latitude: 25.6407, longitude: 84.42845},
+  {latitude: 25.6409, longitude: 84.42775},
+  {latitude: 25.64254, longitude: 84.4243},
+
+  //Dhamawal
+  {latitude: 25.642597, longitude: 84.424347},
+
+  {latitude: 25.64254, longitude: 84.4243},
+  {latitude: 25.64293, longitude: 84.4235},
+  {latitude: 25.64409, longitude: 84.421},
+
+  // Chotka Boda
+  {latitude: 25.644007, longitude: 84.420995},
+
+  {latitude: 25.64409, longitude: 84.421},
+  {latitude: 25.64293, longitude: 84.4235},
+  {latitude: 25.64215, longitude: 84.423},
+  {latitude: 25.64192, longitude: 84.42285},
+
+  //Egarbasawa
+  {latitude: 25.641727, longitude: 84.423277},
+
+  {latitude: 25.64192, longitude: 84.42285},
+  {latitude: 25.641, longitude: 84.42224},
+
+  //badka boda
+  {latitude: 25.641113, longitude: 84.422035},
+
+  //badka boda ka balan
+  {latitude: 25.642828, longitude: 84.417809},
+
+  {latitude: 25.641113, longitude: 84.422035},
+  {latitude: 25.641, longitude: 84.42224},
+  {latitude: 25.64, longitude: 84.4216},
+  {latitude: 25.63943, longitude: 84.4212},
+  {latitude: 25.63923, longitude: 84.421},
+  {latitude: 25.6391, longitude: 84.4207},
+  {latitude: 25.63904, longitude: 84.42054},
+
+  //kurd pe
+  {latitude: 25.639552, longitude: 84.419603},
+
+  //kurd ke balan
+  {latitude: 25.639766, longitude: 84.418213},
+
+  //Sitahal ke khal
+  {latitude: 25.64018, longitude: 84.41583},
+
+  {latitude: 25.639766, longitude: 84.418213},
+  {latitude: 25.639552, longitude: 84.419603},
+  {latitude: 25.63904, longitude: 84.42054},
+  {latitude: 25.6386, longitude: 84.42028},
+  {latitude: 25.638, longitude: 84.41965},
+  {latitude: 25.63772, longitude: 84.419},
+  {latitude: 25.6375, longitude: 84.4186},
+  {latitude: 25.6369, longitude: 84.41765},
+
+  //Chotka nara
+  {latitude: 25.636412, longitude: 84.418189},
+
+  {latitude: 25.6369, longitude: 84.41765},
+  {latitude: 25.6365, longitude: 84.4169},
+  {latitude: 25.637, longitude: 84.416755},
+  {latitude: 25.63745, longitude: 84.4168},
+
+  //pipar tar ke balaan
+  {latitude: 25.637515, longitude: 84.415877},
+
+  {latitude: 25.63745, longitude: 84.4168},
+  {latitude: 25.637, longitude: 84.416755},
+  {latitude: 25.6365, longitude: 84.4169},
+  {latitude: 25.6363, longitude: 84.4162},
+
+  //nara ka balaan
+  {latitude: 25.636134, longitude: 84.415551},
+  {latitude: 25.63615, longitude: 84.41555},
+
+  //badka nara
+  {latitude: 25.635004, longitude: 84.416162},
+
+  //chaturwali
+  {latitude: 25.633934, longitude: 84.416467},
+
+  //paschim rajawali
+  {latitude: 25.633964, longitude: 84.417824},
+
+  //chapatahi
+  {latitude: 25.634712, longitude: 84.420363},
+
+  //baba ka chapatahi
+  {latitude: 25.6350987, longitude: 84.42016},
+
+  {latitude: 25.63475, longitude: 84.42},
+  {latitude: 25.63389, longitude: 84.422},
+
+  //purab rajawali
+  {latitude: 25.633318, longitude: 84.421855},
+
+  //sahebwali
+  {latitude: 25.63266155, longitude: 84.4216376},
+
+  {latitude: 25.6325, longitude: 84.42163},
+  {latitude: 25.63212, longitude: 84.4235},
+  {latitude: 25.633, longitude: 84.4241},
+
+  //sai bigaha
+  {latitude: 25.6323026, longitude: 84.4252458},
+
+  {latitude: 25.633, longitude: 84.4241},
+  {latitude: 25.63415, longitude: 84.4248},
+  {latitude: 25.6342, longitude: 84.4249},
+  {latitude: 25.6344, longitude: 84.42489},
+  {latitude: 25.6357, longitude: 84.4256},
+
+  //badki
+  {latitude: 25.63648, longitude: 84.423477},
+
+  {latitude: 25.6357, longitude: 84.4256},
+  {latitude: 25.6344, longitude: 84.42489},
+  {latitude: 25.6342, longitude: 84.4249},
+  {latitude: 25.63415, longitude: 84.4248},
+  {latitude: 25.6335, longitude: 84.4244},
+  {latitude: 25.63247, longitude: 84.426},
+  {latitude: 25.6316, longitude: 84.427},
+  {latitude: 25.6307, longitude: 84.428},
+  {latitude: 25.6306, longitude: 84.4282},
+  {latitude: 25.6305, longitude: 84.42826},
+  {latitude: 25.6297, longitude: 84.42867},
+  {latitude: 25.62888, longitude: 84.42884},
+  {latitude: 25.62873, longitude: 84.428828},
+  {latitude: 25.62845, longitude: 84.42902},
+  {latitude: 25.6283, longitude: 84.4292},
+  {latitude: 25.627999, longitude: 84.4295},
+  {latitude: 25.6279, longitude: 84.429999},
+  {latitude: 25.62787, longitude: 84.4301},
+  {latitude: 25.6279, longitude: 84.4308},
+  {latitude: 25.62818, longitude: 84.4313},
+  {latitude: 25.62845, longitude: 84.43175},
+];
